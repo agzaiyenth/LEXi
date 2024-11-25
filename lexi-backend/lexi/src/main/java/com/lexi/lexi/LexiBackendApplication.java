49b038b1c49c73d1b0b0a8288b487d681fe3c468
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-11-25 13:15:24 +0530

    Basic Folder Structure

---
+++ lexi-backend/lexi/src/main/java/com/lexi/lexi/LexiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lexi.lexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexiBackendApplication.class, args);
+	}
+
+}
---
+++ lexi-backend/lexi/src/test/java/com/lexi/lexi/LexiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexi.lexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
