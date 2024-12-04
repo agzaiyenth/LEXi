1838109dadd927a4cf98838c054410e4200faeff
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2024-12-04 23:39:25 +0530

    remove old tabs route

--- client/app/(tabs)/learn.tsx
+++
@@ -1,9 +0,0 @@
-// import { View, Text } from 'react-native';
-
-// export default function LearnScreen() {
-//   return (
-//     <View className="flex-1 items-center justify-center">
-//       <Text>Learn Zone Screen</Text>
-//     </View>
-//   );
-// }
