b55a0b30133371eb331afece54e8d0ea87312f3d
Rikas Ilamdeen <rikasilamdeen@gmail.com>
2024-12-26 09:18:36 +0530

    refactor

--- server/src/main/java/com/lexi/predict/repository/UserResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lexi.predict.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lexi.predict.model.UserResponse;
-
-public interface UserResponseRepository extends JpaRepository<UserResponse, Long> {
-}
-
