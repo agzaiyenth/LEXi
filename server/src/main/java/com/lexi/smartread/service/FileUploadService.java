1d4f93b1d4d5cf8727e7f49e2cf9e542b2e7f6a5
Amanda Hansamali <amandahansamali18@gmail.com>
2025-01-04 20:55:39 +0530

    upload file to blob method implemented.

--- server/src/main/java/com/lexi/smartread/service/FileUploadService.java
+++ server/src/main/java/com/lexi/smartread/service/FileUploadService.java
@@ -1,9 +1,11 @@
 package com.lexi.smartread.service;
 
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,6 +34,21 @@ public class FileUploadService {
     }
 
     // Upload file to Azure Blob Storage
+    public String uploadFile(MultipartFile file) throws IOException {
+        // Generate a unique name for the file
+        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
 
+        // Get a reference to the container
+        BlobServiceClient blobServiceClient = getBlobServiceClient();
+        BlobClient blobClient = blobServiceClient.getBlobContainerClient(containerName)
+                .getBlobClient(fileName);
+
+        // Upload the file to the container
+        try (InputStream inputStream = file.getInputStream()) {
+            blobClient.upload(inputStream, file.getSize(), true);  // Overwrite if file exists
+        }
+
+        return fileName; // Return the blob name (file identifier)
+    }
 
 }
